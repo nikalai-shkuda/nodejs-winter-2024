e75fb82e3aa0d7d5d7b76250f99135a74c487e52
Nikalai Shkuda <nikolayshkuda@mail.ru>
2024-12-12 10:57:07 +0300

    Base config; update packages

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(PORT, () =>
+    console.log('Server is running on port: ' + PORT, process.env.NODE_ENV),
+  );
 }
 bootstrap();
