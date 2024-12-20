428660527456ebf8faf1c2f0008a0b335ab01ba3
Nikalai Shkuda <nikolayshkuda@mail.ru>
2024-12-20 20:33:04 +0300

    feat: Init Postgres; Move User module to DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import 'reflect-metadata';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AlbumsModule } from './albums/albums.module';
 import { ArtistsModule } from './artists/artists.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { TracksModule } from './tracks/tracks.module';
+import { User } from './users/users.model';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -16,8 +20,21 @@ import { UsersModule } from './users/users.module';
     FavoritesModule,
     TracksModule,
     UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      entities: [User],
+      logging: true,
+      synchronize: process.env.NODE_ENV === 'development' ? true : false,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,31 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Exclude } from 'class-transformer';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import * as uuid from 'uuid';
 import { randomUUID } from 'src/common/constants';
 import { IUser } from './interfaces/user.interface';
 
+@Entity('users')
 export class User implements IUser {
+  @PrimaryGeneratedColumn('uuid')
   @ApiProperty({
     example: randomUUID,
     description: 'ID',
   })
   readonly id: string = uuid.v4();
 
+  @Column({ unique: true })
   @ApiProperty({ example: 'example', description: 'Login' })
   readonly login: string;
 
+  @Column()
   @Exclude()
   @ApiProperty({ example: 'qwert', description: 'Password' })
   readonly password: string;
 
+  @Column({ default: 1 })
   @ApiProperty({ example: 1, description: 'Password' })
   readonly version: number = 1;
 
+  @Column({ type: 'double precision' })
   @ApiProperty({ example: 1733933613671, description: 'Password' })
   readonly createdAt: number = Date.now();
 
+  @Column({ type: 'double precision' })
   @ApiProperty({ example: 1733933613671, description: 'Password' })
-  readonly updatedAt: number = Date.now();
+  readonly updatedAt: number = this.createdAt;
 
   constructor(partial: Partial<User>) {
     Object.assign(this, partial);
