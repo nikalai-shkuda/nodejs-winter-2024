5ab2eaf2d7c7d161333e5466f90bdc5d64dc1252
Nikalai Shkuda <nikolayshkuda@mail.ru>
2025-01-20 10:54:05 +0300

    fix(Auth): move JwtAuthGuard

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtAuthGuard } from './jwt-auth.guard';
 import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
   controllers: [AuthController],
   imports: [
     UsersModule,
